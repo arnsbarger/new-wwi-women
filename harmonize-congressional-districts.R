7904495f1a5da7e4afa9fa57bdd0e7766b305113
Madison Arnsbarger <madisonarnsbarger@dhcp-137-190-217-105.wireless.weber.edu>
2025-07-11 12:19:35 -0600

    finished harmonizing 65th

--- harmonize-congressional-districts.R
+++ harmonize-congressional-districts.R
@@ -24,129 +24,185 @@ table(shp66_cd$STATENAME, shp66_cd$redist6566) # ID, MI, MT, NY, TX
 
 # Visually compare 63rd and 66th congressional districts
 sort(unique(shp66_cd$ID_STATEDIST))
-
-# Loop through 48 states ####
-states <- sort(unique(shp66_cd$STATENAME))
-for (i in states) {
-  
-  map63 <- 
-    ggplot2::ggplot() + 
-    geom_sf(
-      data = shp63_cd %>% filter(STATENAME == i), 
-      aes(fill=DISTRICT), 
-      color="black", 
-      size=0.5
-    ) +
-    geom_sf_label(
-      data = shp63_cd %>% filter(STATENAME == i), 
-      aes(label=DISTRICT),
-      size=3
-    ) +
-    scale_fill_viridis_d() +
-    guides(fill = "none") + # Remove legend
-    labs(caption = "63") +  # Add bottom-centered title
-    theme_void() +
-    theme(
-      plot.caption = element_text(
-        hjust = 0.5,    # Center horizontally
-        vjust = 1,      # Adjust vertical position (1 = bottom)
-        size = 16,      # Adjust size as desired
-        face = "bold"
-      )
-    )
-  map66 <- 
-    ggplot2::ggplot() + 
-    geom_sf(
-      data = shp66_cd %>% filter(STATENAME == i), 
-      aes(fill=DISTRICT), 
-      color="black", 
-      size=0.5
-    ) +
-    geom_sf_label(
-      data = shp66_cd %>% filter(STATENAME == i), 
-      aes(label=DISTRICT),
-      size=3
-    ) +
-    scale_fill_viridis_d() +
-    guides(fill = "none") + # Remove legend
-    labs(caption = "66") +  # Add bottom-centered title
-    theme_void() +
-    theme(
-      plot.caption = element_text(
-        hjust = 0.5,    # Center horizontally
-        vjust = 1,      # Adjust vertical position (1 = bottom)
-        size = 16,      # Adjust size as desired
-        face = "bold"
-      )
-    )
-  combined <- cowplot::plot_grid(map63, map66, nrow=1)
-  ggsave(paste0("figures/cd_maps/", i, ".pdf"), combined, width = 12, height = 6)
-}
-
-# Special cases! ####
-map63 <- 
-  ggplot2::ggplot() + 
-  geom_sf(
-    # data = shp63_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
-    # data = shp63_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
-    data = shp63_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
-    aes(fill=DISTRICT), 
-    color="black", 
-    size=0.5
-  ) +
-  geom_sf_label(
-    # data = shp63_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
-    # data = shp63_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
-    data = shp63_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
-    aes(label=DISTRICT),
-    size=2
-  ) +
-  scale_fill_viridis_d() +
-  guides(fill = "none") + # Remove legend
-  labs(caption = "63") +  # Add bottom-centered title
-  theme_void() +
-  theme(
-    plot.caption = element_text(
-      hjust = 0.5,    # Center horizontally
-      vjust = 1,      # Adjust vertical position (1 = bottom)
-      size = 16,      # Adjust size as desired
-      face = "bold"
-    )
-  )
-map66 <- 
-  ggplot2::ggplot() + 
-  geom_sf(
-    # data = shp66_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
-    # data = shp66_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
-    data = shp66_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
-    aes(fill=DISTRICT), 
-    color="black", 
-    size=0.5
-  ) +
-  geom_sf_label(
-    # data = shp66_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
-    # data = shp66_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
-    data = shp66_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
-    aes(label=DISTRICT),
-    size=2
-  ) +
-  scale_fill_viridis_d() +
-  guides(fill = "none") + # Remove legend
-  labs(caption = "66") +  # Add bottom-centered title
-  theme_void() +
-  theme(
-    plot.caption = element_text(
-      hjust = 0.5,    # Center horizontally
-      vjust = 1,      # Adjust vertical position (1 = bottom)
-      size = 16,      # Adjust size as desired
-      face = "bold"
-    )
-  )
-combined <- cowplot::plot_grid(map63, map66, nrow=1)
-# ggsave(paste0("figures/cd_maps/Boston.pdf"), combined, width = 12, height = 6) # BOSTON
-# ggsave(paste0("figures/cd_maps/Detroit.pdf"), combined, width = 12, height = 6) # DETROIT
-ggsave(paste0("figures/cd_maps/New York City.pdf"), combined, width = 12, height = 6) # NYC
-
+# 
+# # Loop through 48 states ####
+# states <- sort(unique(shp66_cd$STATENAME))
+# for (i in states) {
+# 
+#   map63 <-
+#     ggplot2::ggplot() +
+#     geom_sf(
+#       data = shp63_cd %>% filter(STATENAME == i),
+#       aes(fill=DISTRICT),
+#       color="black",
+#       size=0.5
+#     ) +
+#     geom_sf_label(
+#       data = shp63_cd %>% filter(STATENAME == i),
+#       aes(label=DISTRICT),
+#       size=3
+#     ) +
+#     scale_fill_viridis_d() +
+#     guides(fill = "none") + # Remove legend
+#     labs(caption = "63") +  # Add bottom-centered title
+#     theme_void() +
+#     theme(
+#       plot.caption = element_text(
+#         hjust = 0.5,    # Center horizontally
+#         vjust = 1,      # Adjust vertical position (1 = bottom)
+#         size = 16,      # Adjust size as desired
+#         face = "bold"
+#       )
+#     )
+#   map65 <-
+#     ggplot2::ggplot() +
+#     geom_sf(
+#       data = shp65_cd %>% filter(STATENAME == i),
+#       aes(fill=DISTRICT),
+#       color="black",
+#       size=0.5
+#     ) +
+#     geom_sf_label(
+#       data = shp65_cd %>% filter(STATENAME == i),
+#       aes(label=DISTRICT),
+#       size=3
+#     ) +
+#     scale_fill_viridis_d() +
+#     guides(fill = "none") + # Remove legend
+#     labs(caption = "65") +  # Add bottom-centered title
+#     theme_void() +
+#     theme(
+#       plot.caption = element_text(
+#         hjust = 0.5,    # Center horizontally
+#         vjust = 1,      # Adjust vertical position (1 = bottom)
+#         size = 16,      # Adjust size as desired
+#         face = "bold"
+#       )
+#     )
+#   map66 <-
+#     ggplot2::ggplot() +
+#     geom_sf(
+#       data = shp66_cd %>% filter(STATENAME == i),
+#       aes(fill=DISTRICT),
+#       color="black",
+#       size=0.5
+#     ) +
+#     geom_sf_label(
+#       data = shp66_cd %>% filter(STATENAME == i),
+#       aes(label=DISTRICT),
+#       size=3
+#     ) +
+#     scale_fill_viridis_d() +
+#     guides(fill = "none") + # Remove legend
+#     labs(caption = "66") +  # Add bottom-centered title
+#     theme_void() +
+#     theme(
+#       plot.caption = element_text(
+#         hjust = 0.5,    # Center horizontally
+#         vjust = 1,      # Adjust vertical position (1 = bottom)
+#         size = 16,      # Adjust size as desired
+#         face = "bold"
+#       )
+#     )
+#   combined <- cowplot::plot_grid(map63, map65, map66, nrow=1)
+#   ggsave(paste0("figures/cd_maps/", i, ".pdf"), combined, width = 12, height = 6)
+# }
+# 
+# # Special cases! ####
+# map63 <-
+#   ggplot2::ggplot() +
+#   geom_sf(
+#     # data = shp63_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
+#     # data = shp63_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
+#     data = shp63_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
+#     aes(fill=DISTRICT),
+#     color="black",
+#     size=0.5
+#   ) +
+#   geom_sf_label(
+#     # data = shp63_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
+#     # data = shp63_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
+#     data = shp63_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
+#     aes(label=DISTRICT),
+#     size=2
+#   ) +
+#   scale_fill_viridis_d() +
+#   guides(fill = "none") + # Remove legend
+#   labs(caption = "63") +  # Add bottom-centered title
+#   theme_void() +
+#   theme(
+#     plot.caption = element_text(
+#       hjust = 0.5,    # Center horizontally
+#       vjust = 1,      # Adjust vertical position (1 = bottom)
+#       size = 16,      # Adjust size as desired
+#       face = "bold"
+#     )
+#   )
+# map65 <-
+#   ggplot2::ggplot() +
+#   geom_sf(
+#     # data = shp65_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
+#     # data = shp65_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
+#     data = shp65_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
+#     aes(fill=DISTRICT),
+#     color="black",
+#     size=0.5
+#   ) +
+#   geom_sf_label(
+#     # data = shp65_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
+#     # data = shp65_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
+#     data = shp65_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
+#     aes(label=DISTRICT),
+#     size=2
+#   ) +
+#   scale_fill_viridis_d() +
+#   guides(fill = "none") + # Remove legend
+#   labs(caption = "65") +  # Add bottom-centered title
+#   theme_void() +
+#   theme(
+#     plot.caption = element_text(
+#       hjust = 0.5,    # Center horizontally
+#       vjust = 1,      # Adjust vertical position (1 = bottom)
+#       size = 16,      # Adjust size as desired
+#       face = "bold"
+#     )
+#   )
+# map66 <-
+#   ggplot2::ggplot() +
+#   geom_sf(
+#     # data = shp66_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
+#     # data = shp66_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) > 12 & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
+#     data = shp66_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
+#     aes(fill=DISTRICT),
+#     color="black",
+#     size=0.5
+#   ) +
+#   geom_sf_label(
+#     # data = shp66_cd %>% filter(STATENAME == "Massachusetts" & as.numeric(DISTRICT) > 7 & as.numeric(DISTRICT) <13), # BOSTON
+#     # data = shp66_cd %>% filter(STATENAME == "Michigan" & as.numeric(DISTRICT) %in% c(1, 2, 6, 7, 13)), # DETROIT
+#     data = shp66_cd %>% filter(STATENAME == "New York" & as.numeric(DISTRICT) %in% 2:24), # NYC
+#     aes(label=DISTRICT),
+#     size=2
+#   ) +
+#   scale_fill_viridis_d() +
+#   guides(fill = "none") + # Remove legend
+#   labs(caption = "66") +  # Add bottom-centered title
+#   theme_void() +
+#   theme(
+#     plot.caption = element_text(
+#       hjust = 0.5,    # Center horizontally
+#       vjust = 1,      # Adjust vertical position (1 = bottom)
+#       size = 16,      # Adjust size as desired
+#       face = "bold"
+#     )
+#   )
+# combined <- cowplot::plot_grid(map63, map65, map66, nrow=1)
+# # ggsave(paste0("figures/cd_maps/Boston.pdf"), combined, width = 12, height = 6) # BOSTON
+# # ggsave(paste0("figures/cd_maps/Detroit.pdf"), combined, width = 12, height = 6) # DETROIT
+# ggsave(paste0("figures/cd_maps/New York City.pdf"), combined, width = 12, height = 6) # NYC
+# 
+# 
+# 
 
 # Based on vision inspection, manually code the 66th district to which each 63rd district corresponds ####
 
@@ -175,28 +231,49 @@ unlinkable <- c(
 
 shp66_cd$linked_to63 <- as.numeric(!shp66_cd$ID_STATEDIST %in% unlinkable)
 
-# WHY AM I NOT DOING THE OPPOSITE??? Why not edit cong66 and use ID_STATEDISTin63, so later i can add ID_STATEDISTin65 ? Also I'm using the 66th id's above.
-# keep "linked_to63" above, and then do "manual_link_to63" below.
+# Manually assign each 66th CD a 63rd CD
+cong66$ID_STATEDISTin63 <- cong66$ID_STATEDIST
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Florida04"] <- "Florida02" # FL 2-4
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Idaho01"] <- "Idaho00b" # FRENCH, Burton Lee
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Idaho02"] <- "Idaho00a" # SMITH, Addison Taylor
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Montana01"] <- "Montana00b" # EVANS, John Morgan
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Montana02"] <- "Montana00a"
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Utah01"] <- "Utah00b"
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Utah02"] <- "Utah00a"
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Washington03"] <- "Washington02" # JOHNSON, Albert
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Washington04"] <- "Washington03" # WA 3-4 
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Washington05"] <- "Washington03" # WA 3-5
+cong66$ID_STATEDISTin63[cong66$ID_STATEDIST=="Michigan13"] <- "Michigan01" # MI 1-13
 
-# Manually assign each 63rd CD a 66th CD
-cong63$ID_STATEDISTin66 <- cong63$ID_STATEDIST
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Florida02"] <- "Florida04" # FL 2-4
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Idaho00b"] <- "Idaho01" # FRENCH, Burton Lee
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Idaho00a"] <- "Idaho02" # SMITH, Addison Taylor
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Montana00b"] <- "Montana01" # EVANS, John Morgan
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Montana00a"] <- "Montana02"
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Utah00b"] <- "Utah01"
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Utah00a"] <- "Utah02"
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Washington02"] <- "Washington03" # JOHNSON, Albert
-cong63$ID_STATEDISTin66[cong63$ID_STATEDIST=="Washington03"] <- "Washington04" # WA 3-4 
-# add rows for CDs that were split in two for 66th congress
-temp <- cong63[cong63$ID_STATEDIST %in% c("Washington03","Michigan01"),]
-temp$ID_STATEDISTin66[temp$ID_STATEDIST=="Washington03"] <- "Washington05" # WA 3-5
-temp$ID_STATEDISTin66[temp$ID_STATEDIST=="Michigan01"] <- "Michigan13"
-cong63 <- rbind(cong63, temp)
-
-cong63$manual_link_to66 <- ifelse(
-  test = (cong63$ID_STATEDIST != cong63$ID_STATEDISTin66),
+cong66$manual_link_to63 <- ifelse(
+  test = (cong66$ID_STATEDIST != cong66$ID_STATEDISTin63) | (cong66$ID_STATEDIST %in% unlinkable),
   yes = 1,
   no = 0
 )
+
+# Drop 66th CDs if too severely redistricted and unlinkable to 65th 
+# ID, MI, MT, NY, TX
+unlinkable <- c(
+  # ID 0-1, 0-2
+  # MI ok
+  # MT 0-1, 0-2
+  "New York04","New York06","New York07","New York08","New York09","New York10","New York15","New York16","New York17","New York18","New York21", 
+  "Texas06","Texas07","Texas08","Texas13","Texas14","Texas15","Texas16","Texas17","Texas18"
+)
+
+shp66_cd$linked_to65 <- as.numeric(!shp66_cd$ID_STATEDIST %in% unlinkable)
+
+# Manually assign each 66th CD a 63rd CD
+cong66$ID_STATEDISTin65 <- cong66$ID_STATEDIST
+cong66$ID_STATEDISTin65[cong66$ID_STATEDIST=="Idaho01"] <- "Idaho00b" # FRENCH, Burton Lee
+cong66$ID_STATEDISTin65[cong66$ID_STATEDIST=="Idaho02"] <- "Idaho00a" # SMITH, Addison Taylor
+cong66$ID_STATEDISTin65[cong66$ID_STATEDIST=="Montana01"] <- "Montana00b" # EVANS, John Morgan
+cong66$ID_STATEDISTin65[cong66$ID_STATEDIST=="Montana02"] <- "Montana00a"
+
+cong66$manual_link_to65 <- ifelse(
+  test = (cong66$ID_STATEDIST != cong66$ID_STATEDISTin65) | (cong66$ID_STATEDIST %in% unlinkable),
+  yes = 1,
+  no = 0
+)
+
+
